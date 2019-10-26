4683832a7f50c52f651b48aaa6b1ebd40d153c96
Concavebark <concavebarkplaysgames@yahoo.com>
2019-10-25 22:34:58 -0400

    Add files via upload

---
+++ lists/BlockList.java
@@ -0,0 +1,11 @@
+package concavebark.moreMinecraftMod.lists;
+
+import net.minecraft.block.Block;
+
+public class BlockList {
+	public static Block bread_ore;
+	public static Block advanced_bread_ore;
+	
+	public static Block tutorial_block;
+	public static Block tutorial_ore, tutorial_ore_nether, tutorial_ore_end;
+}
---
+++ lists/ToolMaterialList.java
@@ -0,0 +1,54 @@
+package concavebark.moreMinecraftMod.lists;
+
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.Ingredient;
+
+public enum ToolMaterialList implements IItemTier{
+	tutorial(10.0f, 9.0f, 800, 3, 25, ItemList.tutorial_item),
+	debug(100.0f, 100.0f, 1000, 3, 25, ItemList.tutorial_item),
+	bread(5.0f, 5.0f, 999, 2, 10, ItemList.breadium);
+
+	private float attackDamage, efficiency;
+	private int durability, harvestLevel, enchantability;
+	private Item repairMaterial;
+	
+	private ToolMaterialList(float attackDamage, float efficiency, int durability, int harvestLevel, int enchantability, Item repairMaterial) {
+		this.attackDamage = attackDamage;
+		this.efficiency = efficiency;
+		this.durability = durability;
+		this.harvestLevel = harvestLevel;
+		this.enchantability = enchantability;
+		this.repairMaterial = repairMaterial;
+	}
+
+	@Override
+	public int getMaxUses() {
+		return durability;
+	}
+
+	@Override
+	public float getEfficiency() {
+		return efficiency;
+	}
+
+	@Override
+	public float getAttackDamage() {
+		return attackDamage;
+	}
+
+	@Override
+	public int getHarvestLevel() {
+		return harvestLevel;
+	}
+
+	@Override
+	public int getEnchantability() {
+		return enchantability;
+	}
+
+	@Override
+	public Ingredient getRepairMaterial() {
+		return Ingredient.fromItems(repairMaterial);
+	}
+}
